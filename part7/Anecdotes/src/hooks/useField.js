49051c4a5ec543052a851fded8318a11359e4530
dannyboi07 <48730475+dannyboi07@users.noreply.github.com>
2022-04-23 23:09:08 +0530

    Add files via upload

---
+++ part7/Anecdotes/src/hooks/useField.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export function useField(type) {
+    const [value, setValue] = useState("");
+
+    function onChange(e) {
+        setValue(e.target.value);
+    };
+
+    return {
+        type,
+        value,
+        onChange
+    };
+};
