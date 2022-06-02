0e9552815b2d1adcc8c3e3e12d2435aa1e7ce4b1
Daniel Chettiar <48730475+dannyboi07@users.noreply.github.com>
2022-06-02 14:49:00 +0530

    Add files via upload

---
+++ part12/socio-dockerized/socio-backend/s3/s3Client.js
@@ -0,0 +1,57 @@
+const {
+	S3Client,
+	GetObjectCommand,
+	PutObjectCommand,
+} = require("@aws-sdk/client-s3");
+const fs = require("fs");
+
+const region = process.env.AWS_S3_REGION;
+const credentials = {
+	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+};
+// Reminder: Currently using keys of IAM user that was created specifically
+// just for accessing the bucket
+
+const s3Client = new S3Client({ credentials, region });
+
+function uploadProfImg(filePath) {
+	const fileReadStream = fs.createReadStream(filePath);
+
+	const uploadParams = {
+		Bucket: process.env.AWS_S3_BUCKET_NAME,
+		Key: `public/profile-pics/${file.filename}`,
+		Body: fileReadStream,
+	};
+
+	return s3Client.send(new PutObjectCommand(uploadParams));
+}
+
+function uploadFile(filePath) {
+	const fileReadStream = fs.createReadStream(filePath);
+
+	const uploadParams = {
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+		Key: `public/post-images/${file.filename}`,
+		Body: fileReadStream,
+	};
+
+	return s3Client.send(new PutObjectCommand(uploadParams));
+}
+
+function getS3Obj(Key) {
+	const getParams = {
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+		Key,
+	};
+
+	return s3Client.send(new GetObjectCommand(getParams));
+}
+
+module.exports = {
+	s3Client,
+	GetObjectCommand,
+	uploadFile,
+	uploadProfImg,
+	getS3Obj,
+};
