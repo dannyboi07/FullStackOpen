edc639046a87346b6dac531cede9135690a63fb7
Daniel Chettiar <48730475+dannyboi07@users.noreply.github.com>
2022-06-02 17:57:39 +0530

    Add files via upload

---
+++ part11/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+// Heroku dynamically sets a port
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static("dist"));
+app.get("/health", (req, res) => {
+	res.send("ok");
+});
+app.get("/version", (req, res) => {
+	res.send("1");
+});
+
+app.listen(PORT, () => {
+	// eslint-disable-next-line no-console
+	console.log(`Server started on port: ${PORT}`);
+});
